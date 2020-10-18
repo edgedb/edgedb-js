6b7e073428622c6f4a4aed9d6775a6482518e3bf
Yury Selivanov <yury@edgedb.com>
2020-10-18 09:29:37 -0700

    -------------------------- wip -=

--- src/reflection/model.ts
+++ src/reflection/model.ts
@@ -46,6 +46,8 @@ export interface LinkDesc<T, C extends Cardinality> {
 // `Property` and `Link` are empty interfaces made so that TS
 // doesn't expand them in the autocomplete.
 const PointerType = Symbol.for("pointer-type");
+const LinkTargetAttr = Symbol.for("link-target");
+export const links = Symbol.for("object-type-links");
 export enum PointerKind {
   link,
   property,
@@ -62,6 +64,48 @@ export interface LinkRef<T extends LinkDesc<any, any>> extends Link {
   kind: PointerKind.link;
   name: string;
   cardinality: Cardinality;
+}
+
+type LL<T extends object> = {
+  [K in keyof T]: T[K];
+};
+
+export interface ObjectType {
+  [links]: string;
+}
+
+export interface Object1 {
+  [links]: [...string[]];
+}
+
+type Unpack<T extends ObjectType> = {
+  [k in keyof T & T[typeof links]]: T[k] extends LinkDesc<infer LT, any>
+    ? LT extends ObjectType
+      ? Unpack<LT>
+      : never
+    : T[k] extends PropertyDesc<infer PT, any>
+    ? PT extends ObjectType
+      ? Unpack<PT>
+      : never
+    : never;
+};
+
+export function Path<T extends ObjectType>(
+  parent: Object1 | null,
+  target: () => object
+): Unpack<T> {
+  const t = target() as Object1;
+  const ret = {parent};
+
+  for (const link of t[links]) {
+    Object.defineProperty(ret, link, {
+      get: (l: string = link): any => {
+        return Path(t, () => (t as any)[l]);
+      },
+    });
+  }
+
+  return ret as any;
 }
 
 type UnpackBoolArg<Arg, T> = Arg extends true
--- t2.ts
+++ t2.ts
@@ -1,27 +1,30 @@
-import * as schema from "./aaa";
+import * as schema from "./aaa2";
 
-const a = schema.test.Issue.shape({
-  name: true,
-  tags: true,
-  number: true,
-  due_date: true,
-  owner: {
-    name: true,
-    todo: {
-      body: true,
-    },
-  },
-  priority: {
-    name: true,
-  },
-  // owner: {
-  //   todo: {
-  //     name: true,
-  //     references: {
-  //       title1: true,
-  //       title2: true,
-  //       id: true,
-  //     },
-  //   },
-  // },
-});
+const b = schema.test.Issue.watchers;
+console.log(schema.test.Issue.kk, "------------", b);
+
+// const a = schema.test.Issue.shape({
+//   name: true,
+//   tags: true,
+//   number: true,
+//   due_date: true,
+//   owner: {
+//     name: true,
+//     todo: {
+//       body: true,
+//     },
+//   },
+//   priority: {
+//     name: true,
+//   },
+//   // owner: {
+//   //   todo: {
+//   //     name: true,
+//   //     references: {
+//   //       title1: true,
+//   //       title2: true,
+//   //       id: true,
+//   //     },
+//   //   },
+//   // },
+// });
