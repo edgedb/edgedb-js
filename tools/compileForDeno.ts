142eb7dc17186ccd53263c6e6c13737f6da29ffd
James Clarke <james@edgedb.com>
2021-03-23 16:45:49 +0000

    Refactor compileForDeno.ts

--- tools/compileForDeno.ts
+++ tools/compileForDeno.ts
@@ -1,46 +1,56 @@
+import {walk, ensureDir} from "https://deno.land/std@0.90.0/fs/mod.ts";
 import {
   join,
   relative,
   dirname,
   basename,
-} from "https://deno.land/std@0.89.0/path/mod.ts";
-import {createRequire} from "https://deno.land/std@0.89.0/node/module.ts";
+} from "https://deno.land/std@0.90.0/path/mod.ts";
+import {createRequire} from "https://deno.land/std@0.90.0/node/module.ts";
+
+const sourceDir = "./src";
+const destDir = "./edgedb-deno";
+const pathRewriteRules = [
+  {match: /^src\/index.node.ts$/, replace: "mod.ts"},
+  {match: /^src\//, replace: "_src/"},
+];
+const injectImports = {
+  imports: ["Buffer", "process"],
+  from: "src/globals.deno.ts",
+};
 
 const require = createRequire(import.meta.url);
 
 const ts = require("typescript");
 
-const targetDir = "./edgedb-deno/_src";
-const injectImports = [
-  {
-    imports: ["Buffer", "process"],
-    from: "./src/globals.deno.ts",
-  },
-];
-
 try {
-  await Deno.remove(targetDir, {recursive: true});
+  await Deno.remove(destDir, {recursive: true});
 } catch {}
 
-async function compileFileForDeno(filePath: string, outPath: string) {
-  const isIndex = filePath.endsWith("index.node.ts");
+const sourceFilePathMap = new Map<string, string>();
 
-  if (filePath.endsWith(".node.ts") && !isIndex) {
+for await (const entry of walk(sourceDir, {includeDirs: false})) {
+  const sourcePath = entry.path;
+  sourceFilePathMap.set(sourcePath, resolveDestPath(sourcePath));
+}
+
+for (const [sourcePath, destPath] of sourceFilePathMap) {
+  compileFileForDeno(sourcePath, destPath);
+}
+
+async function compileFileForDeno(sourcePath: string, destPath: string) {
+  const file = await Deno.readTextFile(sourcePath);
+  await ensureDir(dirname(destPath));
+
+  if (destPath.endsWith(".deno.ts")) {
+    return await Deno.writeTextFile(destPath, file);
+  }
+
+  if (destPath.endsWith(".node.ts")) {
     return;
   }
 
-  if (isIndex) {
-    outPath = join(dirname(outPath), "../mod.ts");
-  }
-
-  const file = await Deno.readTextFile(filePath);
-
-  if (filePath.endsWith(".deno.ts")) {
-    return await Deno.writeTextFile(outPath, file);
-  }
-
   const parsedSource = ts.createSourceFile(
-    basename(filePath),
+    basename(sourcePath),
     file,
     ts.ScriptTarget.Latest,
     false,
@@ -49,34 +59,32 @@ async function compileFileForDeno(filePath: string, outPath: string) {
 
   const rewrittenFile: string[] = [];
   let cursor = 0;
-  let firstNode = true;
+  let isFirstNode = true;
   parsedSource.forEachChild((node: any) => {
-    if (firstNode) {
-      firstNode = false;
+    if (isFirstNode) {
+      isFirstNode = false;
 
-      const injected = injectImports
-        .map(({imports, from}) => {
-          const imported = imports.filter((importName) =>
-            parsedSource.identifiers?.has(importName)
-          );
-          if (!imported.length) {
-            return null;
-          }
-          return `import {${imported.join(", ")}} from "./${relative(
-            dirname(filePath),
-            from
-          )}";`;
-        })
-        .filter((importStr) => importStr);
+      const neededImports = injectImports.imports.filter((importName) =>
+        parsedSource.identifiers?.has(importName)
+      );
 
-      if (injected.length) {
+      if (neededImports.length) {
+        const imports = neededImports.join(", ");
+        const importPath = resolveImportPath(
+          relative(dirname(sourcePath), injectImports.from),
+          sourcePath
+        );
+
+        const importDecl = `import {${imports}} from "${importPath}";\n\n`;
+
         const injectPos =
           node.getLeadingTriviaWidth?.(parsedSource) ?? node.pos;
         rewrittenFile.push(file.slice(cursor, injectPos));
-        rewrittenFile.push(`${injected.join("\n")}\n\n`);
+        rewrittenFile.push(importDecl);
         cursor = injectPos;
       }
     }
+
     if (
       (node.kind === ts.SyntaxKind.ImportDeclaration ||
         node.kind === ts.SyntaxKind.ExportDeclaration) &&
@@ -88,62 +96,56 @@ async function compileFileForDeno(filePath: string, outPath: string) {
       rewrittenFile.push(file.slice(cursor, pos));
       cursor = end;
 
-      const path = file.slice(pos, end);
+      const importPath = file.slice(pos, end);
 
-      if (path.endsWith("./adapter.node")) {
-        rewrittenFile.push(
-          path.replace("./adapter.node", "./adapter.deno.ts")
+      let resolvedImportPath = resolveImportPath(importPath, sourcePath);
+
+      if (resolvedImportPath.endsWith("/adapter.node.ts")) {
+        resolvedImportPath = resolvedImportPath.replace(
+          "/adapter.node.ts",
+          "/adapter.deno.ts"
         );
-        return;
       }
 
-      let resolvedPath = file.slice(pos, end);
-
-      if (!fileExists(join(dirname(filePath), resolvedPath) + ".ts")) {
-        resolvedPath += "/index";
-
-        if (!fileExists(join(dirname(filePath), resolvedPath) + ".ts")) {
-          throw new Error(`Cannot find file: ${path} in ${filePath}`);
-        }
-      }
-
-      if (isIndex) {
-        resolvedPath = "./" + join("_src", resolvedPath);
-      }
-
-      rewrittenFile.push(resolvedPath + ".ts");
+      rewrittenFile.push(resolvedImportPath);
     }
   });
   rewrittenFile.push(file.slice(cursor));
 
-  await Deno.writeTextFile(outPath, rewrittenFile.join(""));
+  await Deno.writeTextFile(destPath, rewrittenFile.join(""));
 }
 
-async function compileDir(dirPath: string, outDirPath: string) {
-  await Deno.mkdir(join(outDirPath), {recursive: true});
+function resolveDestPath(sourcePath: string) {
+  let destPath = sourcePath;
+  for (const rule of pathRewriteRules) {
+    destPath = destPath.replace(rule.match, rule.replace);
+  }
+  return join(destDir, destPath);
+}
 
-  for await (const dirEntry of Deno.readDir(dirPath)) {
-    if (dirEntry.isFile) {
-      const filePath = join(dirPath, dirEntry.name);
+function resolveImportPath(importPath: string, sourcePath: string) {
+  let resolvedPath = join(dirname(sourcePath), importPath);
+  if (!sourceFilePathMap.has(resolvedPath)) {
+    // If importPath doesn't exist, first try appending '.ts'
+    resolvedPath = join(dirname(sourcePath), importPath + ".ts");
 
-      compileFileForDeno(filePath, join(outDirPath, dirEntry.name));
-    }
-    if (dirEntry.isDirectory) {
-      compileDir(
-        join(dirPath, dirEntry.name),
-        join(outDirPath, dirEntry.name)
-      );
+    if (!sourceFilePathMap.has(resolvedPath)) {
+      // If that path doesn't exist, next try appending '/index.ts'
+      resolvedPath = join(dirname(sourcePath), importPath + "/index.ts");
+
+      if (!sourceFilePathMap.has(resolvedPath)) {
+        throw new Error(
+          `Cannot find imported file '${importPath}' in '${sourcePath}'`
+        );
+      }
     }
   }
-}
 
-function fileExists(filePath: string) {
-  try {
-    Deno.statSync(filePath);
-    return true;
-  } catch (e) {
-    return false;
-  }
+  const relImportPath = relative(
+    dirname(sourceFilePathMap.get(sourcePath)!),
+    sourceFilePathMap.get(resolvedPath)!
+  );
+  return relImportPath.startsWith("../")
+    ? relImportPath
+    : "./" + relImportPath;
 }
-
-await compileDir("./src", targetDir);
