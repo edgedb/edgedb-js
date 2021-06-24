8a3040799deb862b0c583e2b289eb4891a3635d1
Colin McDonnell <colin@edgedb.com>
2021-06-23 21:31:12 -0400

    Add merge cardinalities, selectUtil, syntax dir. Split typeutil into its own file.

---
+++ src/reflection/typeutil.ts
@@ -0,0 +1,11 @@
+export namespace typeutil {
+  export type assertEqual<T, Expected> = [T] extends [Expected]
+    ? [Expected] extends [T]
+      ? true
+      : false
+    : false;
+
+  export type depromisify<T> = T extends Promise<infer U> ? depromisify<U> : T;
+  export type identity<T> = T;
+  export type flatten<T> = identity<{[k in keyof T]: T[k]}>;
+}
