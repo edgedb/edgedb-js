d6cda32047f93ff7cec137cd4e5500bae556a8c8
James Clarke <james@edgedb.com>
2021-07-21 12:10:18 +0100

    Update deno stdlib to 0.102.0 + update deno version to 1.12

--- src/adapter.deno.ts
+++ src/adapter.deno.ts
@@ -1,10 +1,10 @@
-import {process} from "https://deno.land/std@0.95.0/node/process.ts";
-import {Buffer} from "https://deno.land/std@0.95.0/node/buffer.ts";
-import * as crypto from "https://deno.land/std@0.95.0/node/crypto.ts";
-import {Sha256, HmacSha256} from "https://deno.land/std@0.95.0/hash/sha256.ts";
-import path from "https://deno.land/std@0.95.0/node/path.ts";
-import EventEmitter from "https://deno.land/std@0.95.0/node/events.ts";
-import util from "https://deno.land/std@0.95.0/node/util.ts";
+import {process} from "https://deno.land/std@0.102.0/node/process.ts";
+import {Buffer} from "https://deno.land/std@0.102.0/node/buffer.ts";
+import * as crypto from "https://deno.land/std@0.102.0/node/crypto.ts";
+import {Sha256, HmacSha256} from "https://deno.land/std@0.102.0/hash/sha256.ts";
+import path from "https://deno.land/std@0.102.0/node/path.ts";
+import EventEmitter from "https://deno.land/std@0.102.0/node/events.ts";
+import util from "https://deno.land/std@0.102.0/node/util.ts";
 
 export {Buffer, path, process, util, crypto};
 
--- src/globals.deno.ts
+++ src/globals.deno.ts
@@ -1,2 +1,2 @@
-export {Buffer} from "https://deno.land/std@0.95.0/node/buffer.ts";
-export {process} from "https://deno.land/std@0.95.0/node/process.ts";
+export {Buffer} from "https://deno.land/std@0.102.0/node/buffer.ts";
+export {process} from "https://deno.land/std@0.102.0/node/process.ts";
