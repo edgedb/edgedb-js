b113f670b9670fc5abbf83151e1040457ccb475a
Colin McDonnell <colin@edgedb.com>
2021-08-01 23:41:42 -0400

    Implement runtime select

--- src/syntax/select.ts
+++ src/syntax/select.ts
@@ -1,37 +1,15 @@
 import {
   BaseExpression,
-  SomeObjectType,
-  // BaseShapeExpression,
-  // BaseShapeType,
-  BaseTypeSet,
-  computeObjectShape,
   ExpressionKind,
-  LinkDesc,
-  linkToTsType,
-  makeSet,
   objectExprToSelectParams,
   ObjectType,
   ObjectTypeExpression,
-  ObjectTypeShape,
   objectTypeToSelectParams,
   Poly,
-  PropertyDesc,
-  propToTsType,
-  setToTsType,
-  shapeElementToTsType,
-  // shapeExprToSelectParams,
-  // ShapeType,
-  // simpleShape,
   TypeKind,
-  TypeSet,
-  typeutil,
-  AnyPoly,
 } from "reflection";
 import {$pathify} from "./path";
-import {toEdgeQL} from "./toEdgeQL";
 
-// simple select
-// object type select
 export enum OrderByDirection {
   ASC = "ASC",
   DESC = "DESC",
@@ -48,25 +26,6 @@ type OrderBy = {
   empty: OrderByEmpty | null;
 };
 
-export type $expr_ShapeSelect<
-  Expr extends ObjectTypeExpression,
-  Params extends object | null,
-  Polys extends Poly[]
-> = BaseExpression<{
-  __element__: ObjectType<
-    `${Expr["__element__"]["__name__"]}_variant`,
-    Expr["__element__"]["__shape__"],
-    Params,
-    Polys
-  >;
-  __cardinality__: Expr["__cardinality__"];
-}> & {
-  __expr__: Expr;
-  __kind__: ExpressionKind.ShapeSelect;
-  __params__: Params;
-  __polys__: Polys;
-};
-
 export type $expr_SimpleSelect<
   Expr extends BaseExpression = BaseExpression
 > = BaseExpression<{
@@ -77,21 +36,22 @@ export type $expr_SimpleSelect<
   __kind__: ExpressionKind.SimpleSelect;
 };
 
-// type mergeObjects<A, B> = typeutil.flatten<A & B>;
-// type mergeObjectsVariadic<T> = T extends []
-//   ? never
-//   : T extends [infer U]
-//   ? U
-//   : T extends [infer A, infer B, ...infer Rest]
-//   ? mergeObjectsVariadic<[mergeObjects<A, B>, ...Rest]>
-//   : never;
-
-// export function shape<
-//   Type extends SomeObjectType,
-//   Params extends objectTypeToSelectParams<Type>
-// >(expr: {__element__: Type}, params: Params) {
-//   return {type: expr.__element__, params};
-// }
+export type $expr_ShapeSelect<
+  Expr extends ObjectTypeExpression,
+  Params extends object | null,
+  Polys extends Poly[]
+> = BaseExpression<{
+  __element__: ObjectType<
+    `${Expr["__element__"]["__name__"]}_shape`,
+    Expr["__element__"]["__shape__"],
+    Params,
+    Polys
+  >;
+  __cardinality__: Expr["__cardinality__"];
+}> & {
+  __expr__: Expr;
+  __kind__: ExpressionKind.ShapeSelect;
+};
 
 export function is<
   Expr extends ObjectTypeExpression,
@@ -100,55 +60,6 @@ export function is<
   return {type: expr.__element__, params};
 }
 
-// export function select<
-//   OT extends ObjectType,
-//   Expr extends {__element__: OT} | {__element__: {__root__: OT}},
-//   Params extends objectTypeToSelectParams<OT>
-// >(expr: Expr, params: Params): $expr_ShapeSelect<Expr, Params>;
-// export function select<
-//   OT extends ObjectType,
-//   Expr extends {__element__: {__root__: OT}},
-//   Params extends objectTypeToSelectParams<OT>
-// >(
-//   expr: Expr,
-//   params: Params
-// ): Expr extends ObjectTypeExpression | BaseShapeExpression
-//   ? $expr_ShapeSelect<Expr, Params>
-//   : never;
-
-// export function select<
-//   Expr extends BaseShapeExpression,
-//   Params extends shapeExprToSelectParams<Expr>
-// >(expr: Expr, params: Params): $expr_ShapeSelect<Expr, Params>;
-
-// objectExprToSelectParams;
-// shapeExprToSelectParams;
-// export function select<
-//   Expr extends ObjectTypeExpression,
-//   Params extends objectTypeToSelectParams<Expr["__element__"]>,
-//   PolyExpr extends ObjectTypeExpression,
-//   Polys extends Poly<PolyExpr>[]
-// >(
-//   expr: Expr,
-//   params: Params,
-//   ...polys: Polys
-// ): $expr_ShapeSelect<Expr, Params, Polys>;
-// export function select<Expr extends ObjectTypeExpression>(
-//   expr: Expr
-// ): $expr_ShapeSelect<Expr, {id: true}, []>;
-// export function select<Expr extends BaseExpression>(
-//   expr: Expr
-// ): $expr_SimpleSelect<Expr>;
-// export function select<
-//   Expr extends BaseShapeExpression,
-//   Params extends shapeExprToParams<Expr>
-//   // PolyExpr extends ObjectTypeExpression,
-//   // Polys extends Poly<PolyExpr>[]
-// >(
-//   expr: Expr,
-//   params: Params
-//   // ...polys: Polys
-// ): Params; //$expr_ShapeSelect<Expr, Params>;
 export function select<Expr extends ObjectTypeExpression>(
   expr: Expr
 ): $expr_ShapeSelect<Expr, {id: true}, []>;
@@ -166,28 +77,56 @@ export function select<
   params: Params,
   ...polys: Polys
 ): $expr_ShapeSelect<Expr, Params, Polys>;
-export function select(expr: any, params?: any, ...polys: any[]) {
+export function select(expr: BaseExpression, params?: any, ...polys: any[]) {
   if (!params) {
-    return $pathify({
-      __element__: (expr as ObjectTypeExpression).__element__,
-      __cardinality__: (expr as ObjectTypeExpression).__cardinality__,
-      __expr__: expr,
-      __kind__: ExpressionKind.SimpleSelect,
-      toEdgeQL,
-    }) as any;
+    if (expr.__element__.__kind__ === TypeKind.object) {
+      const objectExpr: ObjectTypeExpression = expr as any;
+      let defaultParams = {id: true};
+      let defaultPolys = [];
+
+      // if another select expression is passed into select
+      // its shape is inherited
+      if ((objectExpr as any).__kind__ === ExpressionKind.ShapeSelect) {
+        const selectExpr: $expr_ShapeSelect<any, any, any> = objectExpr as any;
+        defaultParams = selectExpr.__element__.__params__;
+        defaultPolys = selectExpr.__element__.__polys__;
+      }
+
+      return $pathify({
+        __kind__: ExpressionKind.ShapeSelect,
+        __element__: {
+          __kind__: TypeKind.object,
+          __name__: `${objectExpr.__element__.__name__}_shape`,
+          __shape__: objectExpr.__element__.__shape__,
+          __params__: defaultParams,
+          __polys__: defaultPolys,
+          __tstype__: undefined as any,
+        },
+        __cardinality__: objectExpr.__cardinality__,
+        __expr__: objectExpr,
+      }) as any;
+    } else {
+      return {
+        __kind__: ExpressionKind.SimpleSelect,
+        __element__: expr.__element__,
+        __cardinality__: expr.__cardinality__,
+        __expr__: expr,
+      };
+    }
   }
+
+  const objExpr: ObjectTypeExpression = expr as any;
   return $pathify({
+    __kind__: ExpressionKind.ShapeSelect,
     __element__: {
-      __root__: (expr as ObjectTypeExpression).__element__,
-      __expr__: expr,
-      __kind__: TypeKind.shape,
-      __name__: expr,
+      __kind__: TypeKind.object,
+      __name__: `${objExpr.__element__.__name__}_shape`,
+      __shape__: objExpr.__element__.__shape__,
       __params__: params,
       __polys__: polys,
-    } as any, //
-    __cardinality__: (expr as ObjectTypeExpression).__cardinality__,
+      __tstype__: undefined as any,
+    },
+    __cardinality__: objExpr.__cardinality__,
     __expr__: expr,
-    __kind__: ExpressionKind.ShapeSelect,
-    toEdgeQL,
-  }) as any;
+  });
 }
