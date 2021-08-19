8fccf2675cdf5388260e0405994c9a0ebd55e497
Colin McDonnell <colin@edgedb.com>
2021-08-19 16:07:22 -0400

    WIP

--- qb/playground.ts
+++ qb/playground.ts
@@ -1,33 +1,11 @@
 // tslint:disable:no-console
-import e from "./generated/example";
+import * as e from "@generated/index";
+import {setupTests, teardownTests} from "./test/setupTeardown";
 
-const skip = e.select(e.int64(10)).limit(2);
-
-const remainingHeros = e
-  .select(e.Hero)
-  .orderBy(e.Hero.id)
-  .offset(skip)
-  .limit(0);
-
-const pageResults = e
-  .select(remainingHeros, {
-    id: true,
-    name: true,
-  })
-  .limit(1);
-
-const query = e.select(e.std.FreeObject, {
-  id: true,
-  skip,
-  remainingHeros,
-  pageResults,
-  nextOffset: e.plus(skip, e.count(pageResults)),
-  hasMore: e.select(e.gt(e.count(remainingHeros), e.int64(12))),
-});
-
-type query = typeof query;
-type params = query["__element__"];
-type returntype = query["__element__"]["__tstype__"];
-console.log(query);
-
+async function run() {
+  await teardownTests();
+  await setupTests();
+  await teardownTests();
+}
+run();
 export {};
--- qb/test/setupTeardown.ts
+++ qb/test/setupTeardown.ts
@@ -1,41 +1,100 @@
 import * as edgedb from "edgedb/src/index.node";
 
+// insert tony
+// insert cap
+// insert spidey
+// insert thanos
+// insert doc ock
+// insert "The Avengers"
+type depromisify<T> = T extends Promise<infer U> ? U : T;
+export type TestData = depromisify<ReturnType<typeof setupTests>>;
 export async function setupTests() {
+  interface Hero {
+    id: string;
+    name: string;
+    secret_identity: string;
+  }
+  interface Villain {
+    id: string;
+    name: string;
+    nemesis: {id: string; name: string};
+  }
+  interface Movie {
+    id: string;
+    title: string;
+    genre: string;
+    characters: {id: string}[];
+  }
   // tslint:disable-next-line: no-console
   console.log(`Seeding database...`);
   const pool = await edgedb.createPool();
-  const ironMan = await pool.queryOne(`INSERT Hero {
+  const ironMan: Hero = await pool.queryOne(`SELECT (INSERT Hero {
   name := "Iron Man",
   secret_identity := "Tony Stark"
-}`);
-  // console.log(ironMan);
+}) {id, name, secret_identity}`);
+  console.log(JSON.stringify(ironMan, null, 2));
 
-  const cap = await pool.queryOne(`INSERT Hero {
+  const cap: Hero = await pool.queryOne(`SELECT (INSERT Hero {
   name := "Captain America",
   secret_identity := "Steve Rogers"
-}`);
-  // console.log(cap);
-  const thanos = await pool.queryOne(
-    `INSERT Villain {
+}) { id, name, secret_identity }`);
+  console.log(JSON.stringify(cap, null, 2));
+  const spidey: Hero = await pool.queryOne(`SELECT (INSERT Hero {
+  name := "Spider-Man",
+  secret_identity := "Peter Parket"
+}) { id, name, secret_identity }`);
+  console.log(JSON.stringify(cap, null, 2));
+  const thanos: Villain = await pool.queryOne(
+    `SELECT (INSERT Villain {
   name := "Thanos",
   nemesis := (SELECT Hero FILTER .id = <uuid>$nemesis_id)
-}`,
+}) { id, name, nemesis: { id, name }}`,
     {nemesis_id: ironMan.id}
   );
+  console.log(JSON.stringify(thanos, null, 2));
+  const docock: Villain = await pool.queryOne(
+    `SELECT (INSERT Villain {
+  name := "Doc Ock",
+  nemesis := (SELECT Hero FILTER .id = <uuid>$nemesis_id)
+}) {id, name, nemesis: { id, name }}`,
+    {nemesis_id: spidey.id}
+  );
+  console.log(JSON.stringify(docock, null, 2));
+  const avengers: Movie = await pool.queryOne(
+    `WITH char_ids := array_unpack(<array<uuid>>$character_ids)
+INSERT Movie {
+  title := "The Avengers",
+  rating := 10,
+  genre := Genre.Action,
+  characters := (SELECT Hero FILTER .id IN char_ids)
+}`,
+    {character_ids: [ironMan.id, cap.id]}
+  );
+  console.log(avengers);
 
-  await pool.close();
-
-  return "done";
+  return {
+    ironMan,
+    cap,
+    spidey,
+    thanos,
+    docock,
+    avengers,
+  };
 }
+
 export async function teardownTests() {
   // tslint:disable-next-line: no-console
   console.log(`Deleting database contents...`);
   const pool = await edgedb.createPool();
-  // await pool.execute(`DELETE Villain;`);
-  // await pool.execute(`DELETE Hero;`);
-  // await pool.execute(`DELETE Movie;`);
-  // await pool.execute(`DELETE Bag;`);
-  // await pool.execute(`DELETE Simple;`);
+
+  await pool.execute(`DELETE \`S p a M\``);
+  await pool.execute(`DELETE A`);
+  await pool.execute(`DELETE Łukasz`);
+  await pool.execute(`DELETE Bag;`);
+  await pool.execute(`DELETE Simple;`);
+  await pool.execute(`DELETE Movie;`);
+  await pool.execute(`DELETE Villain;`);
+  await pool.execute(`DELETE Hero;`);
 
   await pool.close();
 
