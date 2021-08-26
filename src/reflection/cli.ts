886d6f5bf9e6362f0ff3a37c864cb1037b96ff55
Colin McDonnell <colin@edgedb.com>
2021-08-26 00:21:21 -0400

    Add files

---
+++ src/reflection/cli.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import path from "path";
+import {ConnectConfig} from "src/con_utils";
+import {generateQB} from "./generate";
+
+const run = async () => {
+  const [command, ...args] = process.argv.slice(2);
+
+  if (command !== "generate") {
+    throw new Error(`Unknown command: ${command}`);
+  }
+  const cxn: ConnectConfig = {password: ""};
+  while (args.length) {
+    const flag = args.shift();
+    const value = args.shift();
+    if (flag === "-I" || flag === "--instance" || flag === "--dsn") {
+      cxn.dsn = value;
+    } else if (flag === "--database" || flag === "-d") {
+      cxn.database = value;
+    } else if (flag === "--dsn") {
+      cxn.dsn = value;
+    } else if (flag === "--host" || flag === "-h") {
+      cxn.host = value;
+    } else if (flag === "--port" || flag === "-P") {
+      cxn.port = Number(value);
+    } else if (flag === "--username" || flag === "-u") {
+      cxn.user = value;
+    } else if (flag === "--password" || flag === "-p") {
+      cxn.password = value;
+    } else {
+      throw new Error(`Unknown option: ${flag}`);
+    }
+  }
+
+  console.log(JSON.stringify(cxn, null, 2));
+  const TO = path.join(__dirname, "generated/example");
+  await generateQB(TO, cxn.dsn ? cxn.dsn : cxn);
+  process.exit();
+};
+
+run();
