c4258ae5fd2038435337944fd888927f3d6732fe
Colin McDonnell <colin@edgedb.com>
2021-09-13 19:29:52 -0400

    WIP

--- run.ts
+++ run.ts
@@ -3,7 +3,7 @@ import {generateQB} from "./src/reflection/generate";
 
 const run = async () => {
   const TO = path.join(__dirname, "generated/example");
-  await generateQB(TO);
+  await generateQB(TO, {dsn: "qb"});
   process.exit();
 };
 run();
