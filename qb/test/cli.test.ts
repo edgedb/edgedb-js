d22f7642388d51017cc703d8ced2fd65e8d6d33a
Colin McDonnell <colin@edgedb.com>
2021-11-17 18:30:07 -0600

    Extend timeout

--- qb/test/cli.test.ts
+++ qb/test/cli.test.ts
@@ -1,9 +1,6 @@
-import {Client, $} from "edgedb";
 import child_process from "child_process";
-import {fs, exists, readFileUtf8} from "../../src/adapter.node";
 import path from "path";
-import * as tc from "conditional-type-checks";
-import {createClient} from "../../src/pool";
+import {exists, fs, readFileUtf8} from "../../src/adapter.node";
 
 const QBDIR = path.resolve(__dirname, "../dbschema/qbout");
 
@@ -25,12 +22,12 @@ test("basic generate", async () => {
 
   // test TypeScript generation
   console.log(`Generating TS...`);
-  await child_process.execSync(CMD.join(" "));
+  child_process.execSync(CMD.join(" "));
   expect(await exists(path.resolve(QBDIR, "index.ts"))).toEqual(true);
 
   // test JS + ESM
   console.log(`Generating ESM...`);
-  await child_process.execSync([...CMD, "--target esm"].join(" "));
+  child_process.execSync([...CMD, "--target esm"].join(" "));
   expect(await exists(path.resolve(QBDIR, "index.mjs"))).toEqual(true);
   expect(await exists(path.resolve(QBDIR, "index.d.ts"))).toEqual(true);
   const esmFile = await readFileUtf8(path.resolve(QBDIR, "index.mjs"));
@@ -40,14 +37,15 @@ test("basic generate", async () => {
 
   // test JS + ESM
   console.log(`Generating CJS...`);
-  await child_process.execSync([...CMD, "--target cjs"].join(" "));
+  child_process.execSync([...CMD, "--target cjs"].join(" "));
   expect(await exists(path.resolve(QBDIR, "index.js"))).toEqual(true);
   expect(await exists(path.resolve(QBDIR, "index.d.ts"))).toEqual(true);
   const cjsFile = await readFileUtf8(path.resolve(QBDIR, "index.js"));
   expect(cjsFile.includes("require")).toEqual(true);
   expect(cjsFile.includes(`require("./modules/std")`)).toEqual(true);
 
+  console.log(`Done. Removing ${QBDIR}...`);
   // re-generate TS
   // expected for other tests
-  await fs.rmdir(QBDIR, {recursive: true});
-});
+  return await fs.rmdir(QBDIR, {recursive: true});
+}, 60000);
