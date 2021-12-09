e26e8ef25eef963cb0293a675a2c787c5695cf11
James Clarke <james@edgedb.com>
2021-12-09 17:01:03 +0000

    Fix scoped expr caching

--- src/syntax/select.ts
+++ src/syntax/select.ts
@@ -5,6 +5,7 @@ import {
   $scopify,
   Cardinality,
   cardinalityUtil,
+  Expression,
   ExpressionKind,
   LinkDesc,
   ObjectType,
@@ -466,6 +467,8 @@ export type normaliseShape<Shape extends object> = {
   [k in Exclude<keyof Shape, SelectModifierNames>]: normaliseElement<Shape[k]>;
 };
 
+const existingScopes = new Set<Expression>();
+
 export function select<Expr extends ObjectTypeExpression>(
   expr: Expr
 ): $expr_Select<
@@ -570,7 +573,13 @@ export function select(...args: any[]) {
     }
   }
 
+  const cleanScopedExprs = existingScopes.size === 0;
+
   const {modifiers: mods, shape, scope} = resolveShape(shapeGetter, expr);
+
+  if (cleanScopedExprs) {
+    existingScopes.clear();
+  }
 
   const {modifiers, cardinality} = handleModifiers(mods, expr);
   return $expressionify(
@@ -603,7 +612,7 @@ function resolveShape(
 
   const scope =
     expr.__element__.__kind__ === TypeKind.object
-      ? $getScopedExpr(expr as any)
+      ? $getScopedExpr(expr as any, existingScopes)
       : expr;
 
   const selectShape =
