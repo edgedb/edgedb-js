69bd15cf3907ef4ca5a44de5f8775f10414fb0c0
Colin McDonnell <colin@edgedb.com>
2022-01-27 20:29:53 -0800

    Add edgedb-generate module

---
+++ edgedb-generate/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+console.log(
+  `Failure: must install edgedb module.
+
+To generate the EdgeDB query builder, you must have
+the \`edgedb\` package installed as a dependency in
+your local project:
+
+With npm:      npm install edgedb
+With yarn:     yarn add edgedb
+
+Then run \`yarn edgedb-generate\` again.`
+);
