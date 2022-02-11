87830143bd04b4501f4e1d1a957ca5269dc6450b
Colin McDonnell <colin@edgedb.com>
2022-02-11 14:32:16 -0800

    Fix ESM gen bug

--- src/reflection/generate.ts
+++ src/reflection/generate.ts
@@ -114,7 +114,7 @@ export async function generateQB(params: {
     index.addExportStarFrom(null, "./castMaps", true);
     index.addExportStarFrom(null, "./syntax/syntax", true);
     index.addImport({$: true}, "edgedb");
-    index.addExportFrom({createClient: true}, "edgedb", true);
+    index.addExportFrom({createClient: true}, "edgedb");
     index.addStarImport("$syntax", "./syntax/syntax", true);
     index.addStarImport("$op", "./operators", true);
 
