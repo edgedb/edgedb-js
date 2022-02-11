dac16ff8b4d7051d08bdbe9169a124f6edffa5c8
Colin McDonnell <colin@edgedb.com>
2022-02-11 14:07:30 -0800

    Update script, remove edgedb-generate

--- edgeql-js/index.js
+++ edgeql-js/index.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 console.log(
-  `Failure: must install edgedb module.
+  `Failure: "edgedb" module is not installed.
 
 To generate the EdgeDB query builder, you must have
-the \`edgedb\` package installed as a dependency in
+the \`edgedb\` module installed as a dependency in
 your local project:
 
 With npm:      npm install edgedb
 With yarn:     yarn add edgedb
 
-Then run \`yarn edgeql-js\` again.`
+Then run \`npx edgeql-js\` again.`
 );
