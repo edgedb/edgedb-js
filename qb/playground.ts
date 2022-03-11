45c5ea17cbc05cce099152956c734eebd5fd01f7
Colin McDonnell <colin@edgedb.com>
2022-03-10 23:04:28 -0800

    Fix tuple in unless conflict (#298)

--- qb/playground.ts
+++ qb/playground.ts
@@ -7,18 +7,21 @@ import e, * as types from "./dbschema/edgeql-js";
 
 async function run() {
   const {client} = await setupTests();
-  const query = e.params({raw_data: e.json}, params =>
-    e.for(e.json_array_unpack(params.raw_data), item => {
-      return e.select({
-        title: e.cast(e.str, item.title),
-        github_scopes: e.cast(e.array(e.str), item.github_scopes),
-      });
+  const query = e
+    .insert(e.Movie, {
+      title: "The Avengers",
+      rating: 11,
     })
-  );
+    .unlessConflict(movie => ({
+      on: e.tuple([movie.title, movie.profile, movie.id]),
+      else: e.update(movie, () => ({
+        set: {
+          rating: 11,
+        },
+      })),
+    }));
   console.log(query.toEdgeQL());
-  const result = await query.run(client, {
-    raw_data: JSON.stringify([{title: "The Marvels", github_scopes: null}]),
-  });
+  const result = await query.run(client);
   console.log(result);
 }
 
