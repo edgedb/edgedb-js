3912753c06eabfc60f2227fdd9f6009314e7556f
James Clarke <james@edgedb.com>
2022-03-16 20:42:33 +0000

    Make bsd sed happy take 3

--- tools/copySyntaxToDist.sh
+++ tools/copySyntaxToDist.sh
@@ -6,4 +6,5 @@ mv dist/__esm/syntax/*.mjs dist/syntax
 rm -r dist/__esm
 cp src/syntax/*.ts dist/syntax/
 rm -rf dist/syntax/genMock
-sed -i"" "s/\"0.0.0\"/\"$npm_package_version\"/" dist/index.shared*
+sed -i.bak -e "s/\"0.0.0\"/\"$npm_package_version\"/" dist/index.shared*
+rm dist/index.shared*.bak
