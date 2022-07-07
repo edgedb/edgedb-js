e63ce85ff4b4aa7c757ec3b11e37c8422dd6dc48
James Clarke <james@edgedb.com>
2022-07-07 13:29:44 +0100

    Fix function codegen

--- src/reflection/util/genutil.ts
+++ src/reflection/util/genutil.ts
@@ -170,7 +170,7 @@ export function toTSScalarType(
         types,
         opts
       );
-      return frag`${opts.edgedbDatatypePrefix}Range<${tn}>`;
+      return frag`${opts.edgedbDatatypePrefix}edgedb.Range<${tn}>`;
     }
 
     default:
