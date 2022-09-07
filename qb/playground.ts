07e4a8670829aeb6be76b6379040a648b9055c2c
Colin McDonnell <colin@edgedb.com>
2022-09-06 18:45:41 -0700

    Document portable shapes

--- qb/playground.ts
+++ qb/playground.ts
@@ -5,7 +5,17 @@ import e from "./dbschema/edgeql-js";
 async function run() {
   const {client} = await setupTests();
 
-  const query = e.str("asdf");
+  const baseShape = e.shape(e.Movie, m => ({
+    title: true,
+    num_actors: e.count(m),
+  }));
+
+  const query = e.select(e.Movie, m => ({
+    ...baseShape(m),
+    release_year: true,
+    filter: e.op(m.title, "=", "The Avengers"),
+  }));
+
   console.log(query.toEdgeQL());
   const result = await query.run(client);
   console.log(result);
