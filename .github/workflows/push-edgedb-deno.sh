3ad41d42807831858d5975915a67bc8369eaa288
Colin McDonnell <colin@edgedb.com>
2022-09-15 13:30:48 -0700

    Update workflows

--- .github/workflows/push-edgedb-deno.sh
+++ .github/workflows/push-edgedb-deno.sh
@@ -6,7 +6,7 @@ git add . -f
 if ! git diff --cached --exit-code > /dev/null;
 then
   git commit -m "Build from edgedb-js $GITHUB_SHA"
-  # git push
+  git push
 else
   echo 'No changes to release'
 fi
