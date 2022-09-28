97113c65e9abc494f5dfdc7f9b2eb84df558694e
Colin McDonnell <colin@edgedb.com>
2022-09-27 19:45:38 -0700

    Fix tests

--- packages/generate/jest.config.js
+++ packages/generate/jest.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["./dist", "./esm", "./mts", "./cjs", "./deno"],
+  testPathIgnorePatterns: [
+    "./dist",
+    "./esm",
+    "./mts",
+    "./cjs",
+    "./deno",
+    "./queries"
+  ],
   transform: {},
   globals: {
     "ts-jest": {
-      tsConfig: "tsconfig.json",
-    },
-  },
+      tsConfig: "tsconfig.json"
+    }
+  }
 };
