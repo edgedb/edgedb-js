dad90efd37638cff23e5e76f4b6fbd08377a0aa9
Scott Trinh <scott@scotttrinh.com>
2023-04-25 09:35:37 -0400

    Remove deprecated importsNotUsedAsValues tsconfig
    This has become a linting concern instead of a compiler concern. To get
    the same check to ensure we use the `type` import, added an ESLint rule
    that checks. No TSLint rule exists that covered this case.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,10 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   root: true,
   rules: {
-    "@typescript-eslint/no-unnecessary-type-constraint": "warn"
+    "@typescript-eslint/no-unnecessary-type-constraint": "warn",
+    // Svelte doesn't correctly compile if imports of the generated /modules
+    // aren't imported as 'import type' in other parts of the generated
+    // querybuilder, so set this option to ensure we always do that
+    "@typescript-eslint/consistent-type-imports": "error"
   }
 };
