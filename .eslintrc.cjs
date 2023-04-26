59be4314c2888df4a4e91abb0a4253a48b8c1224
Scott Trinh <scott@edgedb.com>
2023-04-26 10:12:44 -0400

    Update dependencies (#578)

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unnecessary-type-constraint": "warn",
+    // Svelte doesn't correctly compile if imports of the generated /modules
+    // aren't imported as 'import type' in other parts of the generated
+    // querybuilder, so set this option to ensure we always do that
+    "@typescript-eslint/consistent-type-imports": "error"
+  }
+};
