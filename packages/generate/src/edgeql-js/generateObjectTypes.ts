460d54f22a9e09c1584910b4ea75ab4df03288b0
Scott Trinh <scott@scotttrinh.com>
2023-04-27 14:39:36 -0400

    Revert changes to `generateObjectTypes`
    The actual changes that used to be here are now in `generateInterfaces`

--- packages/generate/src/edgeql-js/generateObjectTypes.ts
+++ packages/generate/src/edgeql-js/generateObjectTypes.ts
@@ -225,11 +225,12 @@ export const generateObjectTypes = (params: GeneratorParams) => {
       // }
       continue;
     }
-    if (type.intersection_of && type.intersection_of.length) {
+    if (
+      (type.union_of && type.union_of.length) ||
+      (type.intersection_of && type.intersection_of.length)
+    ) {
       continue;
     }
-
-    const isUnionType = !!type.union_of?.length;
 
     const {mod, name} = splitName(type.name);
 
@@ -263,13 +264,9 @@ export const generateObjectTypes = (params: GeneratorParams) => {
 
     // const {module: plainTypeModule} = getPlainTypeModule(type.name);
 
-    // if (!isUnionType) {
-    //   plainTypeModule.types.set(name, getTypeName(type.name, true));
-    // }
+    // plainTypeModule.types.set(name, getTypeName(type.name, true));
     // plainTypeModule.buf.writeln([
-    //   t`${
-    //    !isUnionType ? "export " : ""
-    //   }interface ${getTypeName(type.name)}${
+    //   t`export interface ${getTypeName(type.name)}${
     //     type.bases.length
     //       ? ` extends ${type.bases
     //           .map(({id}) => {
@@ -282,20 +279,19 @@ export const generateObjectTypes = (params: GeneratorParams) => {
     //     type.pointers.length
     //       ? `{\n${type.pointers
     //           .map(pointer => {
-    //             const isOptional =
-    //               pointer.real_cardinality === Cardinality.AtMostOne;
+    //             const isOptional = pointer.card === $.Cardinality.AtMostOne;
     //             return `  ${quote(pointer.name)}${
     //               isOptional ? "?" : ""
     //             }: ${getTSType(pointer)}${
-    //               pointer.real_cardinality === Cardinality.Many ||
-    //               pointer.real_cardinality === Cardinality.AtLeastOne
+    //               pointer.card === $.Cardinality.Many ||
+    //               pointer.card === $.Cardinality.AtLeastOne
     //                 ? "[]"
     //                 : ""
     //             }${isOptional ? " | null" : ""};`;
     //           })
     //           .join("\n")}\n}`
     //       : "{}"
-    //   }\n`,
+    //   }\n`
     // ]);
 
     /////////
@@ -463,11 +459,6 @@ export const generateObjectTypes = (params: GeneratorParams) => {
     /////////
     // generate runtime type
     /////////
-    if (isUnionType) {
-      // union types don't need runtime type
-      continue;
-    }
-
     const literal = getRef(type.name, {prefix: ""});
 
     body.writeln([
