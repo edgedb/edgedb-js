57a5b25da49e901371499e2b7dbcd290c81168fe
PastelStoic <pastelstoic@proton.me>
2023-07-07 15:07:49 -0400

    Added linting rule for deno (#675)

--- packages/generate/src/queries.ts
+++ packages/generate/src/queries.ts
@@ -241,6 +241,10 @@ ${functionBody}
 }
 `;
 
+  const denoImpl = `
+// deno-lint-ignore-file require-await
+${tsImpl}`;
+
   const jsImpl = `async function ${functionName}(client${
     hasArgs ? `, args` : ""
   }) {
@@ -275,7 +279,7 @@ export function ${functionName}(client: Executor${
       return [
         {
           path: `${outputBaseFileName}.ts`,
-          contents: tsImpl,
+          contents: denoImpl,
           imports: tsImports,
           extension: ".ts",
         },
