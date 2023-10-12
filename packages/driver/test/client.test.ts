567d960af977f458beca139ed34a510927bf3b7e
Scott Trinh <scott@scotttrinh.com>
2023-10-12 11:19:23 -0400

    Close client in deno multirange test

--- packages/driver/test/client.test.ts
+++ packages/driver/test/client.test.ts
@@ -1362,11 +1362,19 @@ if (getEdgeDBVersion().major >= 2) {
 if (getEdgeDBVersion().major >= 4) {
   test("fetch: multirange", async () => {
     const client = getClient();
-    const multiRangeRes = await client.query("select <multirange<int32>>$mr;", {
-      mr: new MultiRange([new Range(1, 2)]),
-    });
+    try {
+      const expected = new MultiRange([new Range(1, 2)]);
+      const multiRangeRes = await client.query<MultiRange<number>>(
+        "select <multirange<int32>>$mr;",
+        {
+          mr: expected,
+        }
+      );
 
-    expect(multiRangeRes).toEqual([new MultiRange([new Range(1, 2)])]);
+      expect(multiRangeRes).toEqual([expected]);
+    } finally {
+      await client.close();
+    }
   });
 }
 
