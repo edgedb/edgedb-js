64b388efc1a3c568547f3389898b8ad9559c70db
Scott Trinh <scott@scotttrinh.com>
2023-10-12 10:27:56 -0400

    Add integration test for multirange

---
+++ integration-tests/nightly/multirange.test.ts
@@ -0,0 +1,45 @@
+import type { Client, MultiRange } from "edgedb";
+import e from "./dbschema/edgeql-js";
+import { setupTests, tc, teardownTests } from "./setupTeardown";
+
+import type { WithMultiRange } from "./dbschema/interfaces";
+
+interface BaseObject {
+  id: string;
+}
+interface test_WithMultiRange extends BaseObject {
+  ranges: MultiRange<number>;
+}
+
+describe("multirange", () => {
+  let client: Client;
+  beforeAll(async () => {
+    const setup = await setupTests();
+    ({ client } = setup);
+  });
+
+  afterAll(async () => {
+    await teardownTests(client);
+  }, 10_000);
+
+  test("check generated interfaces", () => {
+    tc.assert<tc.IsExact<WithMultiRange, test_WithMultiRange>>(true);
+  });
+
+  test("inferred return type + literal encoding", async () => {
+    const query = e.select(e.WithMultiRange, () => ({
+      ranges: true,
+    }));
+
+    const result = await query.run(client);
+
+    tc.assert<
+      tc.IsExact<
+        typeof result,
+        {
+          ranges: MultiRange<number>;
+        }[]
+      >
+    >(true);
+  });
+});
