e96ba48f72c363dbd90d8e2170fdc732d0e3221e
James Clarke <james@edgedb.com>
2023-11-01 13:53:29 +0000

    Drop nextjs example

--- examples/auth-next-app/src/actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { auth } from "@/edgedb";
-
-const { signout, emailPasswordSignIn, emailPasswordSignUp } =
-  auth.createServerActions();
-
-export { signout, emailPasswordSignIn, emailPasswordSignUp };
--- examples/auth-next-app/src/edgedb.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "edgedb";
-import createAuth from "@edgedb/auth/helpers/next/app";
-
-export const client = createClient({ tlsSecurity: "insecure" });
-export const auth = createAuth(client, {
-  baseUrl: "http://localhost:3000",
-  authCookieName: "edgedb-auth-session",
-});
