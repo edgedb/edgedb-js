e52de5c634cc02b04d3b8f3ec89b54619a0c94f0
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-11-15 19:16:39 +0100

    Use the configured passwordResetUrl in `auth-nextjs`
    When requesting reset password email, the URL from config is used, but was not really set during `auth` initialization.
    the `resetUrl` param is not supported, so I've removed it from the type.

--- packages/auth-nextjs/src/shared.ts
+++ packages/auth-nextjs/src/shared.ts
@@ -24,6 +24,7 @@ export abstract class NextAuth {
       authCookieName: options.authCookieName ?? "edgedb-session",
       pkceVerifierCookieName:
         options.pkceVerifierCookieName ?? "edgedb-pkce-verifier",
+      passwordResetUrl: options.passwordResetUrl,
     };
   }
 
