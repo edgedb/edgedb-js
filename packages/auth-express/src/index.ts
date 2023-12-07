8a79869afa4307d9dc292c7410e740f4e400424a
Scott Trinh <scott@scotttrinh.com>
2023-12-07 13:24:02 -0500

    Do not create routers, just export middleware
    There is no way to add custom error handling middleware to _existing_
    route definitions, so instead of creating a router, Express consumers
    will use the individually defined router middleware directly so they can
    attach error handling and side-effects and handle successful
    authentication events.

--- packages/auth-express/src/index.ts
+++ packages/auth-express/src/index.ts
@@ -9,7 +9,6 @@ import type { Client } from "edgedb";
 import {
   type Request as ExpressRequest,
   type Response as ExpressResponse,
-  Router,
   type NextFunction,
 } from "express";
 
@@ -140,10 +139,30 @@ export class ExpressAuth extends BaseAuth {
     };
   }
 
-  createRouter() {
-    const router = Router().use(this.options.authRoutesPath);
+  signout = async (
+    req: AuthRequest,
+    res: ExpressResponse,
+    next: NextFunction
+  ) => {
+    try {
+      res.clearCookie(this.options.authCookieName, {
+        httpOnly: true,
+        sameSite: "strict",
+      });
+      req.session = new ExpressAuthSession(this.client, undefined);
+      req.tokenData = undefined;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
 
-    router.get("/oauth", async (req, res, next) => {
+  oAuth = {
+    redirect: async (
+      req: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
       try {
         const provider = new URL(req.url).searchParams.get(
           "provider_name"
@@ -158,6 +177,7 @@ export class ExpressAuth extends BaseAuth {
         res.cookie(this.options.pkceVerifierCookieName, pkceSession.verifier, {
           httpOnly: true,
         });
+        next();
         res.redirect(
           pkceSession.getOAuthUrl(
             provider,
@@ -168,9 +188,13 @@ export class ExpressAuth extends BaseAuth {
       } catch (err) {
         next(err);
       }
-    });
+    },
 
-    router.get("/oauth/callback", async (req: CallbackRequest, res, next) => {
+    callback: async (
+      req: CallbackRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
       try {
         const requestUrl = new URL(req.url);
         const error = requestUrl.searchParams.get("error");
@@ -178,18 +202,24 @@ export class ExpressAuth extends BaseAuth {
           const desc = requestUrl.searchParams.get("error_description");
           throw new Error(error + (desc ? `: ${desc}` : ""));
         }
-
         const code = requestUrl.searchParams.get("code");
-        const isSignUp = requestUrl.searchParams.get("isSignUp") === "true";
-        const verifier = req.cookies[this.options.pkceVerifierCookieName];
+        const verificationEmailSentAt = requestUrl.searchParams.get(
+          "verification_email_sent_at"
+        );
+
         if (!code) {
+          if (verificationEmailSentAt) {
+            req.session = new ExpressAuthSession(this.client, undefined);
+            req.isSignUp = true;
+          }
           throw new Error("no pkce code in response");
         }
+        const verifier = req.cookies[this.options.pkceVerifierCookieName];
         if (!verifier) {
           throw new Error("no pkce verifier cookie found");
         }
-        const tokenData = await (await this.core).getToken(code, verifier);
-
+        const isSignUp = requestUrl.searchParams.get("isSignUp") === "true";
+        const tokenData = await(await this.core).getToken(code, verifier);
         res.cookie(this.options.authCookieName, tokenData.auth_token, {
           httpOnly: true,
           sameSite: "lax",
@@ -202,40 +232,55 @@ export class ExpressAuth extends BaseAuth {
         req.provider = requestUrl.searchParams.get(
           "provider"
         ) as BuiltinOAuthProviderNames;
+        next();
       } catch (err) {
         next(err);
       }
-    });
+    },
+  };
 
-    router.get("/emailpassword/verify", async (req: AuthRequest, res, next) => {
+  builtin = {
+    signIn: async (
+      _: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
       try {
-        const requestUrl = new URL(req.url);
-        const verificationToken =
-          requestUrl.searchParams.get("verification_token");
-        const verifier = req.cookies[this.options.pkceVerifierCookieName];
-        if (!verificationToken) {
-          throw new Error("no verification_token in response");
-        }
-        if (!verifier) {
-          throw new Error("no pkce verifier cookie found");
-        }
-        const tokenData = await (
-          await this.core
-        ).verifyEmailPasswordSignup(verificationToken, verifier);
-        res.cookie(this.options.authCookieName, tokenData.auth_token, {
+        const pkceSession = await this.core.then((core) =>
+          core.createPKCESession()
+        );
+        res.cookie(this.options.pkceVerifierCookieName, pkceSession.verifier, {
           httpOnly: true,
-          sameSite: "strict",
         });
-        res.clearCookie(this.options.pkceVerifierCookieName);
-
-        req.session = new ExpressAuthSession(this.client, tokenData.auth_token);
-        req.tokenData = tokenData;
+        next();
+        res.redirect(pkceSession.getHostedUISigninUrl());
       } catch (err) {
         next(err);
       }
-    });
-
-    router.get("/builtin/callback", async (req: CallbackRequest, res, next) => {
+    },
+    signUp: async (
+      _: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
+      try {
+        const pkceSession = await this.core.then((core) =>
+          core.createPKCESession()
+        );
+        res.cookie(this.options.pkceVerifierCookieName, pkceSession.verifier, {
+          httpOnly: true,
+        });
+        next();
+        res.redirect(pkceSession.getHostedUISignupUrl());
+      } catch (err) {
+        next(err);
+      }
+    },
+    callback: async (
+      req: CallbackRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
       try {
         const requestUrl = new URL(req.url);
         const error = requestUrl.searchParams.get("error");
@@ -273,119 +318,118 @@ export class ExpressAuth extends BaseAuth {
         req.provider = requestUrl.searchParams.get(
           "provider"
         ) as BuiltinOAuthProviderNames;
+        next();
       } catch (err) {
         next(err);
       }
-    });
+    },
+  };
 
-    router.get("/builtin/signin", async (_, res, next) => {
+  emailPassword = {
+    signIn: async (
+      req: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
       try {
-        const pkceSession = await this.core.then((core) =>
-          core.createPKCESession()
+        const [email, password] = _extractParams(
+          req.body,
+          ["email", "password"],
+          "email or password missing from request body"
         );
-        res.cookie(this.options.pkceVerifierCookieName, pkceSession.verifier, {
-          httpOnly: true,
-        });
-        res.redirect(pkceSession.getHostedUISigninUrl());
-      } catch (err) {
-        next(err);
-      }
-    });
-
-    router.get("/builtin/signup", async (_, res, next) => {
-      try {
-        const pkceSession = await this.core.then((core) =>
-          core.createPKCESession()
+        const tokenData = await(await this.core).signinWithEmailPassword(
+          email,
+          password
         );
-        res.cookie(this.options.pkceVerifierCookieName, pkceSession.verifier, {
+        res.cookie(this.options.authCookieName, tokenData.auth_token, {
           httpOnly: true,
+          sameSite: "strict",
         });
-        res.redirect(pkceSession.getHostedUISignupUrl());
+        req.session = new ExpressAuthSession(this.client, tokenData.auth_token);
+        req.tokenData = tokenData;
+        next();
       } catch (err) {
         next(err);
       }
-    });
-
-    router.get("/signout", async (req: SessionRequest, res, next) => {
+    },
+    signUp: async (
+      req: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
       try {
-        res.clearCookie(this.options.authCookieName);
-        req.session = new ExpressAuthSession(this.client, undefined);
-      } catch (err) {
-        next(err);
-      }
-    });
-
-    router.post(
-      "/emailpassword/signin",
-      async (req: AuthRequest, res, next) => {
-        try {
-          const [email, password] = _extractParams(
-            req.body,
-            ["email", "password"],
-            "email or password missing from request body"
-          );
-          const tokenData = await (
-            await this.core
-          ).signinWithEmailPassword(email, password);
-          res.cookie(this.options.authCookieName, tokenData.auth_token, {
+        const [email, password] = _extractParams(
+          req.body,
+          ["email", "password"],
+          "email or password missing from request body"
+        );
+        const result = await (
+          await this.core
+        ).signupWithEmailPassword(
+          email,
+          password,
+          `${this._authRoute}/emailpassword/verify`
+        );
+        res.cookie(this.options.pkceVerifierCookieName, result.verifier, {
+          httpOnly: true,
+          sameSite: "strict",
+        });
+        if (result.status === "complete") {
+          res.cookie(this.options.authCookieName, result.tokenData.auth_token, {
             httpOnly: true,
             sameSite: "strict",
           });
           req.session = new ExpressAuthSession(
             this.client,
-            tokenData.auth_token
+            result.tokenData.auth_token
           );
-          req.tokenData = tokenData;
-        } catch (err) {
-          next(err);
+          req.tokenData = result.tokenData;
+        } else {
+          req.session = new ExpressAuthSession(this.client, undefined);
         }
+        next();
+      } catch (err) {
+        next(err);
       }
-    );
-
-    router.post(
-      "/emailpassword/signup",
-      async (req: AuthRequest, res, next) => {
-        try {
-          const [email, password] = _extractParams(
-            req.body,
-            ["email", "password"],
-            "email or password missing from request body"
-          );
-          const result = await (
-            await this.core
-          ).signupWithEmailPassword(
-            email,
-            password,
-            `${this._authRoute}/emailpassword/verify`
-          );
-          res.cookie(this.options.pkceVerifierCookieName, result.verifier, {
-            httpOnly: true,
-            sameSite: "strict",
-          });
-          if (result.status === "complete") {
-            res.cookie(
-              this.options.authCookieName,
-              result.tokenData.auth_token,
-              {
-                httpOnly: true,
-                sameSite: "strict",
-              }
-            );
-            req.session = new ExpressAuthSession(
-              this.client,
-              result.tokenData.auth_token
-            );
-            req.tokenData = result.tokenData;
-          } else {
-            req.session = new ExpressAuthSession(this.client, undefined);
-          }
-        } catch (err) {
-          next(err);
+    },
+    verify: async (
+      req: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
+      try {
+        const requestUrl = new URL(req.url);
+        const verificationToken =
+          requestUrl.searchParams.get("verification_token");
+        const verifier = req.cookies[this.options.pkceVerifierCookieName];
+        if (!verificationToken) {
+          throw new Error("no verification_token in response");
         }
-      }
-    );
+        if (!verifier) {
+          throw new Error("no pkce verifier cookie found");
+        }
+        const tokenData = await(await this.core).verifyEmailPasswordSignup(
+          verificationToken,
+          verifier
+        );
+        res.cookie(this.options.authCookieName, tokenData.auth_token, {
+          httpOnly: true,
+          sameSite: "strict",
+        });
+        res.clearCookie(this.options.pkceVerifierCookieName);
 
-    router.post("/emailpassword/send-reset-email", async (req, res, next) => {
+        req.session = new ExpressAuthSession(this.client, tokenData.auth_token);
+        req.tokenData = tokenData;
+        next();
+      } catch (err) {
+        next(err);
+      }
+    },
+    sendPasswordResetEmail: async (
+      req: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
       try {
         if (!this.options.passwordResetPath) {
           throw new Error(`'passwordResetPath' option not configured`);
@@ -409,148 +453,63 @@ export class ExpressAuth extends BaseAuth {
           sameSite: "strict",
         });
         res.status(204);
+        next();
       } catch (err) {
         next(err);
       }
-    });
-
-    router.post(
-      "/emailpassword/reset-password",
-      async (req: AuthRequest, res, next) => {
-        try {
-          const verifier = req.cookies[this.options.pkceVerifierCookieName];
-          if (!verifier) {
-            throw new Error("no pkce verifier cookie found");
-          }
-          const [resetToken, password] = _extractParams(
-            req.body,
-            ["reset_token", "password"],
-            "reset_token or password missing from request body"
-          );
-
-          const tokenData = await (
-            await this.core
-          ).resetPasswordWithResetToken(resetToken, verifier, password);
-          res.cookie(this.options.authCookieName, tokenData.auth_token, {
-            httpOnly: true,
-            sameSite: "strict",
-          });
-          res.clearCookie(this.options.pkceVerifierCookieName);
-          req.session = new ExpressAuthSession(
-            this.client,
-            tokenData.auth_token
-          );
-          req.tokenData = tokenData;
-        } catch (err) {
-          next(err);
+    },
+    resetPassword: async (
+      req: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
+      try {
+        const verifier = req.cookies[this.options.pkceVerifierCookieName];
+        if (!verifier) {
+          throw new Error("no pkce verifier cookie found");
         }
+        const [resetToken, password] = _extractParams(
+          req.body,
+          ["reset_token", "password"],
+          "reset_token or password missing from request body"
+        );
+
+        const tokenData = await(await this.core).resetPasswordWithResetToken(
+          resetToken,
+          verifier,
+          password
+        );
+        res.cookie(this.options.authCookieName, tokenData.auth_token, {
+          httpOnly: true,
+          sameSite: "strict",
+        });
+        res.clearCookie(this.options.pkceVerifierCookieName);
+        req.session = new ExpressAuthSession(this.client, tokenData.auth_token);
+        req.tokenData = tokenData;
+        next();
+      } catch (err) {
+        next(err);
       }
-    );
-
-    router.post(
-      "/emailpassword/resend-verification-email",
-      async (req, res, next) => {
-        try {
-          const [verificationToken] = _extractParams(
-            req.body,
-            ["verification_token"],
-            "verification_token missing from request body"
-          );
-          (await this.core).resendVerificationEmail(verificationToken);
-          res.status(204);
-        } catch (err) {
-          next(err);
-        }
+    },
+    resendVerificationEmail: async (
+      req: AuthRequest,
+      res: ExpressResponse,
+      next: NextFunction
+    ) => {
+      try {
+        const [verificationToken] = _extractParams(
+          req.body,
+          ["verification_token"],
+          "verification_token missing from request body"
+        );
+        (await this.core).resendVerificationEmail(verificationToken);
+        res.status(204);
+        next();
+      } catch (err) {
+        next(err);
       }
-    );
-
-    return router;
-  }
-
-  async signout(res: ExpressResponse) {
-    res.clearCookie(this.options.authCookieName, {
-      httpOnly: true,
-      sameSite: "strict",
-    });
-    res.locals.session = new ExpressAuthSession(this.client, undefined);
-  }
-
-  async emailPasswordSignIn(
-    res: ExpressResponse,
-    { email, password }: { email: string; password: string }
-  ) {
-    const tokenData = await (
-      await this.core
-    ).signinWithEmailPassword(email, password);
-    res.cookie(this.options.authCookieName, tokenData.auth_token, {
-      httpOnly: true,
-      sameSite: "strict",
-    });
-    return tokenData;
-  }
-
-  async emailPasswordSignUp(
-    res: ExpressResponse,
-    { email, password }: { email: string; password: string }
-  ) {
-    const result = await (
-      await this.core
-    ).signupWithEmailPassword(
-      email,
-      password,
-      `${this._authRoute}/emailpassword/verify`
-    );
-    res.cookie(this.options.pkceVerifierCookieName, result.verifier, {
-      httpOnly: true,
-      sameSite: "strict",
-    });
-    if (result.status === "complete") {
-      res.cookie(this.options.authCookieName, result.tokenData.auth_token, {
-        httpOnly: true,
-        sameSite: "strict",
-      });
-      return result.tokenData;
-    }
-    return null;
-  }
-
-  async emailPasswordSendPasswordResetEmail({ email }: { email: string }) {
-    if (!this.options.passwordResetPath) {
-      throw new Error(`'passwordResetPath' option not configured`);
-    }
-    return (await this.core).sendPasswordResetEmail(
-      email,
-      new URL(this.options.passwordResetPath, this.options.baseUrl).toString()
-    );
-  }
-
-  async emailPasswordResetPassword(
-    req: ExpressRequest,
-    res: ExpressResponse,
-    { reset_token, password }: { reset_token: string; password: string }
-  ) {
-    const verifier = req.cookies[this.options.pkceVerifierCookieName];
-    if (!verifier) {
-      throw new Error("no pkce verifier cookie found");
-    }
-    const tokenData = await (
-      await this.core
-    ).resetPasswordWithResetToken(reset_token, verifier, password);
-    res.cookie(this.options.authCookieName, tokenData.auth_token, {
-      httpOnly: true,
-      sameSite: "strict",
-    });
-    res.clearCookie(this.options.pkceVerifierCookieName);
-    return tokenData;
-  }
-
-  async emailPasswordResendVerificationEmail({
-    verification_token,
-  }: {
-    verification_token: string;
-  }) {
-    return (await this.core).resendVerificationEmail(verification_token);
-  }
+    },
+  };
 }
 
 export default function createExpressAuth(
