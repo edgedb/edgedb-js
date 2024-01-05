31ac57469a500f30b27fea35de478879d23a6acc
Scott Trinh <scott@scotttrinh.com>
2024-01-05 17:15:41 -0500

    In scalar constructors, prefer Omit
    - Delta: -15.40%
    - Delta: -7.44%
    - Delta: -4.43%
    - Delta: -3.57%
    - Delta: -2.77%
    - Delta: -16.90%
    - Delta: -1.48%
    - Delta: -2.57%
    - Delta: -2.46%
    - Delta: -2.56%
    - Delta: -2.55%

--- packages/generate/src/syntax/typesystem.ts
+++ packages/generate/src/syntax/typesystem.ts
@@ -31,8 +31,8 @@ export type BaseTypeTuple = typeutil.tupleOf<BaseType>;
 
 export interface ScalarType<
   Name extends string = string,
-  TsType extends any = any,
-  TsArgType extends any = TsType,
+  TsType = any,
+  TsArgType = TsType,
   TsConstType extends TsType = TsType
 > extends BaseType {
   __kind__: TypeKind.scalar;
@@ -47,13 +47,14 @@ export type scalarTypeWithConstructor<
   ExtraTsTypes = never
 > = S & {
   // tslint:disable-next-line
-  <T extends S["__tstype__"] | ExtraTsTypes>(val: T): $expr_Literal<
-    ScalarType<
-      S["__name__"],
-      S["__tstype__"],
-      S["__tsargtype__"],
-      T extends S["__tstype__"] ? T : S["__tstype__"]
-    >
+  <
+    T extends S["__tstype__"] | ExtraTsTypes,
+  >(
+    val: T
+  ): $expr_Literal<
+    Omit<S, "__tsconsttype__"> & {
+      __tsconsttype__: T extends S["__tstype__"] ? T : S["__tstype__"];
+    }
   >;
 };
 
