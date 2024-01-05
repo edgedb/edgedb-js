595d99969244b3d89a84c89084ae0539c0d48c70
Scott Trinh <scott@scotttrinh.com>
2024-01-05 10:01:17 -0500

    Add attest benches for select expressions

---
+++ integration-tests/lts/bench.ts
@@ -0,0 +1,98 @@
+import { bench } from "@arktype/attest";
+
+import e from "./dbschema/edgeql-js";
+
+bench("select: scalar", () => {
+  const query = e.select(e.int32(42));
+  return {} as typeof query;
+}).types([1263, "instantiations"]);
+
+bench("select: free object", () => {
+  const query = e.select({ meaning: e.int32(42) });
+  return {} as typeof query;
+}).types([2120, "instantiations"]);
+
+bench("select: id only", () => {
+  const query = e.select(e.User, () => ({ id: true }));
+  return {} as typeof query;
+}).types([3642, "instantiations"]);
+
+bench("select: filtered", () => {
+  const query = e.select(e.User, () => ({
+    filter_single: { id: e.uuid("123") },
+  }));
+  return {} as typeof query;
+}).types([5384, "instantiations"]);
+
+bench("select: nested", () => {
+  const user = e.select(e.User, () => ({
+    filter_single: { id: e.uuid("123") },
+  }));
+  const query = e.select(user, () => ({ id: true }));
+
+  return {} as typeof query;
+}).types([7412, "instantiations"]);
+
+bench("select: complex", () => {
+  const query = e.select(e.Movie, () => ({
+    id: true,
+    characters: (char) => ({
+      name: true,
+      "@character_name": true,
+      filter: e.op(char["@character_name"], "=", "Tony Stark"),
+    }),
+  }));
+  return {} as typeof query;
+}).types([6339, "instantiations"]);
+
+bench("select: with filter", () => {
+  const query = e.select(e.Hero, (hero) => ({
+    name: true,
+    villains: {
+      id: true,
+      name: true,
+    },
+    filter_single: e.op(hero.name, "=", "Peter Parker"),
+  }));
+  return {} as typeof query;
+}).types([98669, "instantiations"]);
+
+bench("select: with order", () => {
+  const query = e.select(e.Hero, (hero) => ({
+    name: true,
+    villains: (v) => ({
+      id: true,
+      name: true,
+      order_by: v.name,
+    }),
+    filter_single: e.op(hero.name, "=", "Peter Parker"),
+  }));
+  return {} as typeof query;
+}).types([98963, "instantiations"]);
+
+bench("select: with limit", () => {
+  const query = e.select(e.Hero, (hero) => ({
+    name: true,
+    villains: () => ({
+      id: true,
+      name: true,
+      limit: 1,
+    }),
+    filter_single: e.op(hero.name, "=", "Peter Parker"),
+  }));
+  return {} as typeof query;
+}).types([98694, "instantiations"]);
+
+bench("select: with offset", () => {
+  const query = e.select(e.Hero, (hero) => ({
+    name: true,
+    villains: (v) => ({
+      id: true,
+      name: true,
+      offset: 1,
+    }),
+    filter_single: e.op(hero.name, "=", "Peter Parker"),
+  }));
+  return {} as typeof query;
+}).types([98730, "instantiations"]);
+
