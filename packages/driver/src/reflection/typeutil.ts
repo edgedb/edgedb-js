5848b86200b3e21623d7a7ba71d611bbed9a9899
Scott Trinh <scott@scotttrinh.com>
2024-01-09 16:19:23 -0500

    Slightly cheaper "flatten"

--- packages/driver/src/reflection/typeutil.ts
+++ packages/driver/src/reflection/typeutil.ts
@@ -7,7 +7,7 @@ export namespace typeutil {
 
   export type depromisify<T> = T extends Promise<infer U> ? depromisify<U> : T;
   export type identity<T> = T;
-  export type flatten<T> = identity<{ [k in keyof T]: T[k] }>;
+  export type flatten<T> = { [k in keyof T]: T[k] } & unknown;
   export type tupleOf<T> = [T, ...T[]] | [];
   export type writeable<T> = { -readonly [P in keyof T]: T[P] };
 
