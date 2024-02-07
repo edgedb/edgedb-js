8a04c73101832c41095895f435a3e2a335c37f42
beerose <alexsandra.sikora@gmail.com>
2024-02-07 15:16:31 +0100

    Bump @edgedb/auth-nextjs version

--- packages/create/src/recipes/nextjs/index.ts
+++ packages/create/src/recipes/nextjs/index.ts
@@ -102,7 +102,7 @@ const recipe: Recipe<NextjsOptions> = {
         lint: "next lint",
       },
       dependencies: {
-        ...(useEdgeDBAuth ? { "@edgedb/auth-nextjs": "^0.1.0-beta.1" } : {}),
+        ...(useEdgeDBAuth ? { "@edgedb/auth-nextjs": "^0.1.0" } : {}),
         edgedb: "^1.4.1",
         react: "^18",
         "react-dom": "^18",
