d7434c72ab9a69539d750b0317c5e184307488e0
Aleksandra <alexsandra.sikora@gmail.com>
2024-02-07 16:41:30 +0100

    Create and apply initial migration in the @edgedb/create CLI (#854)

--- packages/create/src/cli.ts
+++ packages/create/src/cli.ts
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
+import * as p from "@clack/prompts";
+import pc from "picocolors";
+
 import { baseRecipe, recipes as _recipes } from "./recipes/index.js";
 
 async function main() {
+  p.intro("Welcome to the EdgeDB Create CLI 🚀");
+
   const baseOptions = await baseRecipe.getOptions();
   const recipeOptions: any[] = [];
 
@@ -18,6 +23,16 @@ async function main() {
   for (let i = 0; i < recipes.length; i++) {
     await recipes[i].apply(baseOptions, recipeOptions[i]);
   }
+
+  p.outro(`\
+Your EdgeDB project has been initialized! 🚀
+
+Enter your project directory using: ${pc.green(
+    `cd ${baseOptions.projectName}`
+  )} 
+Follow the instructions in the ${pc.green("README.md")} file to get started.
+
+Need help? Join our community at ${pc.green("https://edgedb.com/community")}`);
 }
 
 await main();
