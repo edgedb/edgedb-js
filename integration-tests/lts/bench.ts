4d8c92f912d77a7a00b2f8b209bc25e141543032
Scott Trinh <scott@scotttrinh.com>
2024-03-25 10:59:11 -0400

    Update benchmarks and remove expect-error
    Significant performance improvements observed in the latest benchmark tests:
    
    - `e.literal: scalar`: -77.80%.
    - `e.literal: array literal`: -45.89%
    - `e.literal: named tuple literal`: -33.47%
    - `e.tuple: named tuple literal`: -20.70%
    - `e.literal: tuple literal`: -42.10%
    - `e.literal: array of tuples`: -35.18%
    
    Minor performance deltas were observed in other benchmarks, with most
    under 1% change from the baseline. Notably, `e.array: array of tuples`
    showed a -9.96% delta, and `params select` had a -6.34% delta,
    indicating significant improvements.

--- integration-tests/lts/bench.ts
+++ integration-tests/lts/bench.ts
@@ -5,49 +5,49 @@ import e from "./dbschema/edgeql-js";
 bench("e.literal: scalar", () => {
   const lit = e.literal(e.int32, 42);
   return {} as typeof lit;
-}).types([4468, "instantiations"]);
+}).types([992, "instantiations"]);
 
 bench("e.int32: scalar", () => {
   const lit = e.int32(42);
   return {} as typeof lit;
-}).types([558, "instantiations"]);
+}).types([556, "instantiations"]);
 
 bench("e.str: scalar", () => {
   const lit = e.str("abcd");
   return {} as typeof lit;
-}).types([897, "instantiations"]);
+}).types([894, "instantiations"]);
 
 bench("e.literal: array literal", () => {
   const lit = e.literal(e.array(e.str), ["abcd"]);
   return {} as typeof lit;
-}).types([7568, "instantiations"]);
+}).types([4095, "instantiations"]);
 
 bench("e.array: array literal", () => {
   const lit = e.array([e.str("abcd")]);
   return {} as typeof lit;
-}).types([2606, "instantiations"]);
+}).types([2599, "instantiations"]);
 
 bench("e.literal: named tuple literal", () => {
   const lit = e.literal(e.tuple({ str: e.str }), {
     str: "asdf",
   });
   return {} as typeof lit;
-}).types([16528, "instantiations"]);
+}).types([10996, "instantiations"]);
 
 bench("e.tuple: named tuple literal", () => {
   const lit = e.tuple({ str: e.str("asdf") });
   return {} as typeof lit;
-}).types([9831, "instantiations"]);
+}).types([7796, "instantiations"]);
 
 bench("e.literal: tuple literal", () => {
   const lit = e.literal(e.tuple([e.str, e.int32]), ["asdf", 42]);
   return {} as typeof lit;
-}).types([13125, "instantiations"]);
+}).types([7600, "instantiations"]);
 
 bench("e.tuple: tuple literal", () => {
   const lit = e.tuple([e.str("asdf"), e.int32(42)]);
   return {} as typeof lit;
-}).types([4889, "instantiations"]);
+}).types([4836, "instantiations"]);
 
 bench("e.literal: array of tuples", () => {
   const lit = e.literal(e.array(e.tuple([e.str, e.int32])), [
@@ -55,7 +55,7 @@ bench("e.literal: array of tuples", () => {
     ["qwer", 43],
   ]);
   return {} as typeof lit;
-}).types([15721, "instantiations"]);
+}).types([10191, "instantiations"]);
 
 bench("e.array: array of tuples", () => {
   const lit = e.array([
@@ -63,7 +63,7 @@ bench("e.array: array of tuples", () => {
     e.tuple([e.str("qwer"), e.int32(43)]),
   ]);
   return {} as typeof lit;
-}).types([22894, "instantiations"]);
+}).types([20613, "instantiations"]);
 
 bench("base type: array", () => {
   const baseType = e.array(e.str);
@@ -73,29 +73,29 @@ bench("base type: array", () => {
 bench("base type: named tuple", () => {
   const baseType = e.tuple({ str: e.str });
   return {} as typeof baseType;
-}).types([3545, "instantiations"]);
+}).types([3564, "instantiations"]);
 
 bench("select: scalar", () => {
   const query = e.select(e.int32(42));
   return {} as typeof query;
-}).types([1177, "instantiations"]);
+}).types([1173, "instantiations"]);
 
 bench("select: free object", () => {
   const query = e.select({ meaning: e.int32(42) });
   return {} as typeof query;
-}).types([2033, "instantiations"]);
+}).types([2027, "instantiations"]);
 
 bench("select: id only", () => {
   const query = e.select(e.User, () => ({ id: true }));
   return {} as typeof query;
-}).types([3702, "instantiations"]);
+}).types([3687, "instantiations"]);
 
 bench("select: filtered", () => {
   const query = e.select(e.User, () => ({
     filter_single: { id: e.uuid("123") },
   }));
   return {} as typeof query;
-}).types([5039, "instantiations"]);
+}).types([5102, "instantiations"]);
 
 bench("select: nested", () => {
   const user = e.select(e.User, () => ({
@@ -104,7 +104,7 @@ bench("select: nested", () => {
   const query = e.select(user, () => ({ id: true }));
 
   return {} as typeof query;
-}).types([6057, "instantiations"]);
+}).types([6118, "instantiations"]);
 
 bench("select: complex", () => {
   const query = e.select(e.Movie, () => ({
@@ -116,7 +116,7 @@ bench("select: complex", () => {
     }),
   }));
   return {} as typeof query;
-}).types([6374, "instantiations"]);
+}).types([6352, "instantiations"]);
 
 bench("select: with filter", () => {
   const query = e.select(e.Hero, (hero) => ({
@@ -128,7 +128,7 @@ bench("select: with filter", () => {
     filter_single: e.op(hero.name, "=", "Peter Parker"),
   }));
   return {} as typeof query;
-}).types([6447, "instantiations"]);
+}).types([6428, "instantiations"]);
 
 bench("select: with order", () => {
   const query = e.select(e.Hero, (hero) => ({
@@ -141,7 +141,7 @@ bench("select: with order", () => {
     filter_single: e.op(hero.name, "=", "Peter Parker"),
   }));
   return {} as typeof query;
-}).types([6786, "instantiations"]);
+}).types([6765, "instantiations"]);
 
 bench("select: with limit", () => {
   const query = e.select(e.Hero, (hero) => ({
@@ -154,7 +154,7 @@ bench("select: with limit", () => {
     filter_single: e.op(hero.name, "=", "Peter Parker"),
   }));
   return {} as typeof query;
-}).types([6510, "instantiations"]);
+}).types([6490, "instantiations"]);
 
 bench("select: with offset", () => {
   const query = e.select(e.Hero, (hero) => ({
@@ -167,7 +167,7 @@ bench("select: with offset", () => {
     filter_single: e.op(hero.name, "=", "Peter Parker"),
   }));
   return {} as typeof query;
-}).types([6553, "instantiations"]);
+}).types([6533, "instantiations"]);
 
 bench("params select", () => {
   const query = e.params({ name: e.str }, (params) =>
@@ -181,4 +181,4 @@ bench("params select", () => {
     }))
   );
   return {} as typeof query;
-}).types([12010, "instantiations"]);
+}).types([11248, "instantiations"]);
