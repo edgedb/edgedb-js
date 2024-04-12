70b537a63e410f35bba2135536fdb3343223f690
Scott Trinh <scott@scotttrinh.com>
2024-04-12 10:01:33 -0400

    Update type benchmarks

--- integration-tests/lts/bench.ts
+++ integration-tests/lts/bench.ts
@@ -32,12 +32,12 @@ bench("e.literal: named tuple literal", () => {
     str: "asdf",
   });
   return {} as typeof lit;
-}).types([10765, "instantiations"]);
+}).types([10767, "instantiations"]);
 
 bench("e.tuple: named tuple literal", () => {
   const lit = e.tuple({ str: e.str("asdf") });
   return {} as typeof lit;
-}).types([7564, "instantiations"]);
+}).types([7565, "instantiations"]);
 
 bench("e.literal: tuple literal", () => {
   const lit = e.literal(e.tuple([e.str, e.int32]), ["asdf", 42]);
@@ -47,7 +47,7 @@ bench("e.literal: tuple literal", () => {
 bench("e.tuple: tuple literal", () => {
   const lit = e.tuple([e.str("asdf"), e.int32(42)]);
   return {} as typeof lit;
-}).types([4836, "instantiations"]);
+}).types([4837, "instantiations"]);
 
 bench("e.literal: array of tuples", () => {
   const lit = e.literal(e.array(e.tuple([e.str, e.int32])), [
@@ -73,7 +73,7 @@ bench("base type: array", () => {
 bench("base type: named tuple", () => {
   const baseType = e.tuple({ str: e.str });
   return {} as typeof baseType;
-}).types([3564, "instantiations"]);
+}).types([3565, "instantiations"]);
 
 bench("select: scalar", () => {
   const query = e.select(e.int32(42));
