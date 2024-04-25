59bf5135f2e62be2b047fa35511533fb5d667be2
Scott Trinh <scott@scotttrinh.com>
2024-04-25 08:51:17 -0600

    Have async generator generate typed values

--- packages/ai/src/types.ts
+++ packages/ai/src/types.ts
@@ -24,3 +24,54 @@ export interface RAGRequest {
   query: string;
   stream?: boolean;
 }
+
+export type MessageStart = {
+  type: "message_start";
+  message: {
+    role: "assistant" | "system" | "user";
+    id: string;
+    model: string;
+  };
+};
+
+export type ContentBlockStart = {
+  type: "conent_block_start";
+  index: number;
+  content_block: {
+    text: string;
+    type: "text";
+  };
+};
+
+export type ContentBlockDelta = {
+  type: "content_block_delta";
+  delta: {
+    type: "text_delta";
+    text: string;
+  };
+  index: number;
+};
+
+export type ContentBlockStop = {
+  type: "content_block_stop";
+  index: number;
+};
+
+export type MessageDelta = {
+  type: "message_delta";
+  delta: {
+    stop_reason: "stop";
+  };
+};
+
+export type MessageStop = {
+  type: "message_stop";
+};
+
+export type StreamingMessage =
+  | MessageStart
+  | ContentBlockStart
+  | ContentBlockDelta
+  | ContentBlockStop
+  | MessageDelta
+  | MessageStop;
