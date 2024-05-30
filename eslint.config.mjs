e6bae42591b0a575e8d11f974d08efbd40c4b2a8
Scott Trinh <scott@scotttrinh.com>
2024-05-30 12:24:58 -0400

    Use the files setup instead
    This ensures that in-editor tooling picks up the same configuration as
    the lint we run on CI. Also shuffles the rules around a bit to not use
    the stylistic rules in the driver and generator.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,32 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+const DRIVER_PATH = "packages/driver/src/**/*.?(m)ts";
+const GENERATE_PATH = "packages/generate/src/**/*.?(m)ts";
+const FEATURE_LIBRARIES_PATH = "packages/!(driver|generate)/src/**/*.?(m)ts";
+const INTEGRATION_TESTS_PATH = "integration-tests/*/*.test.?(m)ts";
+
+const ALL_PATHS = [
+  DRIVER_PATH,
+  GENERATE_PATH,
+  FEATURE_LIBRARIES_PATH,
+  INTEGRATION_TESTS_PATH,
+];
+
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
   {
-    files: ["packages/*/src/**/*.ts"],
-    languageOptions: {
-      parser: tseslint.parser,
-      // parserOptions: {
-      //   project: true,
-      // },
-    },
+    ...eslint.configs.recommended,
+    files: ALL_PATHS,
     rules: {
+      ...eslint.configs.recommended.rules,
+      "no-empty-function": "off",
+    },
+  },
+  ...tseslint.configs.recommended.map((config) => ({
+    ...config,
+    files: ALL_PATHS,
+    rules: {
+      ...config.rules,
       "@typescript-eslint/no-unnecessary-type-constraint": "warn",
       // Svelte doesn't correctly compile if imports of the generated /modules
       // aren't imported as 'import type' in other parts of the generated
@@ -23,8 +36,6 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-function": "warn",
       "@typescript-eslint/ban-ts-comment": "warn",
-      "no-empty-function": "off",
-      "@typescript-eslint/class-literal-property-style": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
@@ -37,10 +48,14 @@ export default tseslint.config(
           ignoreRestSiblings: true,
         },
       ],
-      // some stylistic rules results with errors, turn them off for now
-      "@typescript-eslint/consistent-indexed-object-style": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/no-empty-interface": "off",
     },
-  },
+  })),
+  ...tseslint.configs.stylistic.map((config) => ({
+    ...config,
+    files: [FEATURE_LIBRARIES_PATH, INTEGRATION_TESTS_PATH],
+    rules: {
+      ...config.rules,
+      "@typescript-eslint/class-literal-property-style": "off",
+    },
+  })),
 );
