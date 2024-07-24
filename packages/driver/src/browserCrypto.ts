8153d8551a717302bfd898b4e1f7d7d36143d438
Scott Trinh <scott@scotttrinh.com>
2024-07-24 10:17:49 -0400

    Avoid needing `this` in crypto utils

--- packages/driver/src/browserCrypto.ts
+++ packages/driver/src/browserCrypto.ts
@@ -1,35 +1,37 @@
 import type { CryptoUtils } from "./utils";
+async function makeKey(key: Uint8Array): Promise<CryptoKey> {
+  return await crypto.subtle.importKey(
+    "raw",
+    key,
+    {
+      name: "HMAC",
+      hash: { name: "SHA-256" },
+    },
+    false,
+    ["sign"],
+  );
+}
+
+async function randomBytes(size: number): Promise<Uint8Array> {
+  return crypto.getRandomValues(new Uint8Array(size));
+}
+
+async function H(msg: Uint8Array): Promise<Uint8Array> {
+  return new Uint8Array(await crypto.subtle.digest("SHA-256", msg));
+}
+
+async function HMAC(
+  key: Uint8Array | CryptoKey,
+  msg: Uint8Array,
+): Promise<Uint8Array> {
+  const cryptoKey =
+    key instanceof Uint8Array ? ((await makeKey(key)) as CryptoKey) : key;
+  return new Uint8Array(await crypto.subtle.sign("HMAC", cryptoKey, msg));
+}
 
 export const cryptoUtils: CryptoUtils = {
-  async makeKey(key: Uint8Array): Promise<CryptoKey> {
-    return await crypto.subtle.importKey(
-      "raw",
-      key,
-      {
-        name: "HMAC",
-        hash: { name: "SHA-256" },
-      },
-      false,
-      ["sign"],
-    );
-  },
-
-  async randomBytes(size: number): Promise<Uint8Array> {
-    return crypto.getRandomValues(new Uint8Array(size));
-  },
-
-  async H(msg: Uint8Array): Promise<Uint8Array> {
-    return new Uint8Array(await crypto.subtle.digest("SHA-256", msg));
-  },
-
-  async HMAC(
-    key: Uint8Array | CryptoKey,
-    msg: Uint8Array,
-  ): Promise<Uint8Array> {
-    const cryptoKey =
-      key instanceof Uint8Array
-        ? ((await this.makeKey(key)) as CryptoKey)
-        : key;
-    return new Uint8Array(await crypto.subtle.sign("HMAC", cryptoKey, msg));
-  },
+  makeKey,
+  randomBytes,
+  H,
+  HMAC,
 };
