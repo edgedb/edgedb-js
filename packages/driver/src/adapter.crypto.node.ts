2f053575329be933a905bb255c968b627bdf2fbc
Scott Trinh <scott@edgedb.com>
2024-07-24 12:03:33 -0400

    Prefer node:crypto (#1064)

--- packages/driver/src/adapter.crypto.node.ts
+++ packages/driver/src/adapter.crypto.node.ts
@@ -2,12 +2,22 @@ import type { CryptoUtils } from "./utils";
 
 let cryptoUtils: CryptoUtils;
 
-if (typeof crypto === "undefined") {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  cryptoUtils = require("./nodeCrypto").cryptoUtils;
-} else {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  cryptoUtils = require("./browserCrypto").cryptoUtils;
+function loadCrypto() {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require("node:crypto");
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    cryptoUtils = require("./nodeCrypto").cryptoUtils;
+  } catch (_) {
+    if (typeof globalThis.crypto !== "undefined") {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      cryptoUtils = require("./browserCrypto").cryptoUtils;
+    } else {
+      throw new Error("No crypto implementation found");
+    }
+  }
 }
 
-export default cryptoUtils;
+loadCrypto();
+
+export default cryptoUtils!;
