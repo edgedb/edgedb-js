342156784b9be3e8897bc2b38ca6f3fc67f240dd
Dijana Pavlovic <dijana@edgedb.com>
2024-11-27 19:22:34 +0100

    Use ts types in ai lib for chunk types instead of zod schema

--- packages/ai/src/types.ts
+++ packages/ai/src/types.ts
@@ -1,6 +1,36 @@
-import { z } from "zod";
-
 export type ChatParticipantRole = "system" | "user" | "assistant" | "tool";
+
+export interface EdgeDBSystemMessage {
+  role: "system";
+  content: string;
+}
+
+export interface EdgeDBUserMessage {
+  role: "user";
+  content: { type: "text"; text: string }[];
+}
+
+export interface EdgeDBAssistantMessage {
+  role: "assistant";
+  content: string;
+  tool_calls?: {
+    id: string;
+    type: "function";
+    function: { name: string; arguments: string };
+  }[];
+}
+
+export interface EdgeDBToolMessage {
+  role: "tool";
+  content: string;
+  tool_call_id: string;
+}
+
+export type EdgeDBMessage =
+  | EdgeDBSystemMessage
+  | EdgeDBUserMessage
+  | EdgeDBAssistantMessage
+  | EdgeDBToolMessage;
 
 export type Prompt =
   | { name: string; custom?: EdgeDBMessage[] }
@@ -40,109 +70,86 @@ export function isPromptRequest(
   return "prompt" in request;
 }
 
-export type EdgeDBMessage =
-  | EdgeDBSystemMessage
-  | EdgeDBUserMessage
-  | EdgeDBAssistantMessage
-  | EdgeDBToolMessage;
-
-export interface EdgeDBSystemMessage {
-  role: "system";
-  content: string;
-}
-
-export interface EdgeDBUserMessage {
-  role: "user";
-  content: { type: "text"; text: string }[];
-}
-
-export interface EdgeDBAssistantMessage {
-  role: "assistant";
-  content: string;
-  tool_calls?: {
+export interface MessageStart {
+  type: "message_start";
+  message: {
     id: string;
-    type: "function";
-    function: { name: string; arguments: string };
-  }[];
+    model: string;
+    role: "assistant" | "system" | "user"; //todo check this;
+    usage?: {
+      prompt_tokens: number;
+      completion_tokens: number;
+    } | null;
+  };
 }
 
-export interface EdgeDBToolMessage {
-  role: "tool";
-  content: string;
-  tool_call_id: string;
+export interface ContentBlockStart {
+  type: "content_block_start";
+  index: number;
+  content_block:
+    | {
+        type: "text";
+        text: string;
+      }
+    | {
+        type: "tool_use";
+        id?: string | null;
+        name: string;
+        args?: string | null;
+      };
 }
 
-export type StreamingMessage = z.infer<typeof _edgedbRagChunkSchema>;
+export interface ContentBlockDelta {
+  type: "content_block_delta";
+  index: number;
+  delta:
+    | {
+        type: "text_delta";
+        text: string;
+      }
+    | {
+        type: "tool_call_delta";
+        args: string;
+      };
+  logprobs?: {
+    tokens: string[];
+    token_logprobs: number[];
+    top_logprobs: Record<string, number>[] | null;
+  } | null;
+}
 
-const _edgedbRagChunkSchema = z.discriminatedUnion("type", [
-  z.object({
-    type: z.literal("message_start"),
-    message: z.object({
-      id: z.string(),
-      model: z.string(),
-      role: z.enum(["assistant", "system", "user"]),
-      usage: z
-        .object({
-          prompt_tokens: z.number(),
-          completion_tokens: z.number(),
-        })
-        .nullish(),
-    }),
-  }),
-  z.object({
-    type: z.literal("content_block_start"),
-    index: z.number(),
-    content_block: z.discriminatedUnion("type", [
-      z.object({
-        type: z.literal("text"),
-        text: z.string(),
-      }),
-      z.object({
-        type: z.literal("tool_use"),
-        id: z.string().nullish(),
-        name: z.string(),
-        args: z.string().nullish(),
-      }),
-    ]),
-  }),
-  z.object({
-    type: z.literal("content_block_delta"),
-    index: z.number(),
-    delta: z.discriminatedUnion("type", [
-      z.object({
-        type: z.literal("text_delta"),
-        text: z.string(),
-      }),
-      z.object({
-        type: z.literal("tool_call_delta"),
-        args: z.string(), // partial json
-      }),
-    ]),
-    logprobs: z
-      .object({
-        tokens: z.array(z.string()),
-        token_logprobs: z.array(z.number()),
-        top_logprobs: z.array(z.record(z.string(), z.number())).nullable(),
-      })
-      .nullish(),
-  }),
-  z.object({
-    type: z.literal("content_block_stop"),
-    index: z.number(),
-  }),
-  z.object({
-    type: z.literal("message_delta"),
-    delta: z.object({ stop_reason: z.string() }),
-    usage: z.object({ completion_tokens: z.number() }).nullish(),
-  }),
-  z.object({
-    type: z.literal("message_stop"),
-  }),
-  z.object({
-    type: z.literal("error"),
-    error: z.object({
-      type: z.string(),
-      message: z.string(),
-    }),
-  }),
-]);
+export interface ContentBlockStop {
+  type: "content_block_stop";
+  index: number;
+}
+
+export interface MessageDelta {
+  type: "message_delta";
+  delta: {
+    stop_reason: string;
+  };
+  usage?: {
+    completion_tokens: number;
+  };
+}
+
+export interface MessageStop {
+  type: "message_stop";
+}
+
+export interface MessageError {
+  type: "error";
+  error: {
+    type: string;
+    message: string;
+  };
+}
+
+export type StreamingMessage =
+  | MessageStart
+  | ContentBlockStart
+  | ContentBlockDelta
+  | ContentBlockStop
+  | MessageDelta
+  | MessageStop
+  | MessageError;
