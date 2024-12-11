cc744052fad728edb7968cc99833869e68f03b5c
Dijana Pavlovic <dijana@edgedb.com>
2024-12-11 11:08:49 +0100

    auth-nextjs: rename to gel

--- packages/auth-nextjs/src/pages/server.ts
+++ packages/auth-nextjs/src/pages/server.ts
@@ -1,5 +1,5 @@
-import { type Client } from "edgedb";
-import { type TokenData } from "@edgedb/auth-core";
+import { type Client } from "gel";
+import { type TokenData } from "@gel/auth-core";
 import {
   type BuiltinProviderNames,
   type CreateAuthRouteHandlers,
@@ -8,7 +8,7 @@ import {
   NextAuthSession,
 } from "../shared";
 
-export * from "@edgedb/auth-core/errors";
+export * from "@gel/auth-core/errors";
 export {
   NextAuthSession,
   type NextAuthOptions,
