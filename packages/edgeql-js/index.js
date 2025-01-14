a07ba67d024b3af6b5d85bbf9f5c2eb4cff87df4
Dijana Pavlovic <dijana@edgedb.com>
2025-01-14 16:09:44 +0100

    Rename edgedb to gel

--- packages/edgeql-js/index.js
+++ packages/edgeql-js/index.js
@@ -3,15 +3,15 @@
 console.log(
   `Failure: The \`npx edgeql-js\` command is no longer supported.
 
-To generate the EdgeDB query builder, install \`@edgedb/generate\`
+To generate the Gel query builder, install \`@gel/generate\`
 package as a dev dependency in your local project. This package implements
-a set of code generators for EdgeDB (including the query builder).
+a set of code generators for Gel (including the query builder).
 
-  $ npm install -D @edgedb/generate      (npm)
-  $ yarn add -D @edgedb/generate         (yarn)
+  $ npm install -D @gel/generate      (npm)
+  $ yarn add -D @gel/generate         (yarn)
 
 Then run the following command to generate the query builder.
 
-  $ npx @edgedb/generate edgeql-js
-`
+  $ npx @gel/generate edgeql-js
+`,
 );
