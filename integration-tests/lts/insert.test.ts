57c88cabc751051b609996cd64f08effc03d393e
James Clarke <james@edgedb.com>
2025-02-07 14:46:09 +0000

    Fix querybuilder `toEdgeQL` when doing 'insert many-to-one, select one' queries using `e.with` (#1191)
    Fixes a bug where we were not also checking the child exprs of the
    additional valid scopes added in this fix:
    https://github.com/edgedb/edgedb-js/pull/1154.
    
    Also fixes bug where scope validity was not being checked in the correct
    order when some expressions in the `e.with` refs array are children of
    other exprs.

--- integration-tests/lts/insert.test.ts
+++ integration-tests/lts/insert.test.ts
@@ -432,4 +432,84 @@ describe("insert", () => {
 
     await query.run(client);
   });
+
+  test("insert many-to-one and select one", async () => {
+    const edgeql = e
+      .params(
+        {
+          name: e.str,
+          nemeses: e.array(e.tuple({ name: e.str })),
+        },
+        (params) => {
+          const hero = e.insert(e.Hero, {
+            name: params.name,
+          });
+          const villains = e.for(e.array_unpack(params.nemeses), (nemesis) => {
+            return e.insert(e.Villain, {
+              name: nemesis.name,
+              nemesis: hero,
+            });
+          });
+
+          return e.with([villains], e.select(hero));
+        },
+      )
+      .toEdgeQL();
+
+    // Also test including `hero` in the `with` refs
+    assert.equal(
+      edgeql,
+      e
+        .params(
+          {
+            name: e.str,
+            nemeses: e.array(e.tuple({ name: e.str })),
+          },
+          (params) => {
+            const hero = e.insert(e.Hero, {
+              name: params.name,
+            });
+            const villains = e.for(
+              e.array_unpack(params.nemeses),
+              (nemesis) => {
+                return e.insert(e.Villain, {
+                  name: nemesis.name,
+                  nemesis: hero,
+                });
+              },
+            );
+
+            return e.with([hero, villains], e.select(hero));
+          },
+        )
+        .toEdgeQL(),
+    );
+    assert.equal(
+      edgeql,
+      e
+        .params(
+          {
+            name: e.str,
+            nemeses: e.array(e.tuple({ name: e.str })),
+          },
+          (params) => {
+            const hero = e.insert(e.Hero, {
+              name: params.name,
+            });
+            const villains = e.for(
+              e.array_unpack(params.nemeses),
+              (nemesis) => {
+                return e.insert(e.Villain, {
+                  name: nemesis.name,
+                  nemesis: hero,
+                });
+              },
+            );
+
+            return e.with([villains, hero], e.select(hero));
+          },
+        )
+        .toEdgeQL(),
+    );
+  });
 });
